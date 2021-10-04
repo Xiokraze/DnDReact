3136ce18ad71542a80077ffffaeb78a784d8a842
Joshua Worthington <Xiokraze@gmail.com>
2021-10-04 18:22:50 -0500

    Removed unused css.

--- dndreact/src/App.js
+++ dndreact/src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import React, { Suspense } from "react";
 import ActivityIndicator from "./Components/ActivityIndicator/ActivityIndicator";
